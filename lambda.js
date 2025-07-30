8fddad65bd427f69690bdef838234955a85f1be7
Jeremy Zongker <jeremy@zongker.net>
2025-07-30 13:57:36 -0500

    Converting stage to lowercase

--- lambda.js
+++ lambda.js
@@ -6,7 +6,7 @@ const { Environment } = require("./dist/src/helpers/Environment");
 const checkPool = async () => {
   if (!Environment.connectionString) {
     await Environment.init(process.env.APP_ENV);
-    //Pool.initPool();
+    Pool.initPool();
   }
 };
 
