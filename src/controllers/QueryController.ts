38b2c966824a66eb5687f90a265cde1c07d12442
Jeremy Zongker <jeremy@zongker.net>
2026-01-14 10:14:00 -0600

    ESM updates

--- src/controllers/QueryController.ts
+++ src/controllers/QueryController.ts
@@ -6,6 +6,11 @@ import { WorkflowHelper } from "../helpers/WorkflowHelper.js";
 
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 @controller("/query")
 export class QueryController extends AskBaseController {
