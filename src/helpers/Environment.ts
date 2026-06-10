c44087d002c511006f53f2ef2ccbc317f399de7c
Jeremy Zongker <jeremy@zongker.net>
2026-06-10 01:00:46 -0500

    Updated helpers

--- src/helpers/Environment.ts
+++ src/helpers/Environment.ts
@@ -1,13 +1,4 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
 import { AwsHelper, EnvironmentBase } from "@churchapps/apihelper";
-
-// ESM compatible __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export class Environment extends EnvironmentBase {
   static membershipApi: string;
@@ -17,16 +8,8 @@ export class Environment extends EnvironmentBase {
   static openRouterApiKey: string;
 
   static async init(environment: string) {
-    let file = "dev.json";
-    if (environment === "staging") file = "staging.json";
-    if (environment === "prod") file = "prod.json";
-
-    const relativePath = "../../config/" + file;
-    const physicalPath = path.resolve(__dirname, relativePath);
-
-    const json = fs.readFileSync(physicalPath, "utf8");
-    const data = JSON.parse(json);
-    await this.populateBase(data, "askApi", environment);
+    // No demo.json in this project; demo runs on the dev config.
+    const data = await this.initBase(environment, { appName: "askApi", fileMap: { demo: "dev.json" } });
 
     this.membershipApi = data.membershipApi;
     this.messagingApi = data.messagingApi;
